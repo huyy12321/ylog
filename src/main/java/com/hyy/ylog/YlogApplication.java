a0b2a749e37077bfdac18989b5b2d1575272937e
hyy <577892771@qq.com>
2020-10-10 13:46:25 +0800

    Initial commit

---
+++ src/main/java/com/hyy/ylog/YlogApplication.java
@@ -0,0 +1,13 @@
+package com.hyy.ylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YlogApplication.class, args);
+    }
+
+}
